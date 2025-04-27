1a87e948936df0411f51446b8f9a1fd90dea9e89
Constantine <tmail99@gmail.com>
2025-04-27 14:44:26 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/dr2cc/URLsShortener.git/internal/handlers"
+	"github.com/dr2cc/URLsShortener.git/internal/storage"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	storageInstance := storage.NewStorage()
+
+	mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
+	mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
+
+	http.ListenAndServe(":8080", mux)
+}
