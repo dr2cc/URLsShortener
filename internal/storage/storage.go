a33771eb965e5b7c3defd681bef3588b10b9206b
Constantine <tmail99@gmail.com>
2025-05-07 16:09:47 +0300

    Все готово к тестам storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,13 +2,6 @@ package storage
 
 import (
 	"errors"
-	"fmt"
-	"io"
-	"math/rand"
-	"net/http"
-	"regexp"
-	"strings"
-	"time"
 )
 
 type Storager interface {
@@ -38,86 +31,6 @@ func (s *URLStorage) GetURL(uid string) (string, error) {
 		return uid, errors.New("URL with such id doesn't exist")
 	}
 	return e, nil
-}
-
-func generateShortURL(urlList *URLStorage, longURL string) string {
-
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	runes := []rune(longURL)
-	r.Shuffle(len(runes), func(i, j int) {
-		runes[i], runes[j] = runes[j], runes[i]
-	})
-
-	reg := regexp.MustCompile(`[^a-zA-Zа-яА-Я0-9]`)
-	//[:11] здесь сокращаю строку
-	id := reg.ReplaceAllString(string(runes[:11]), "")
-
-	MakeEntry(urlList, id, longURL)
-
-	return "/" + id
-}
-
-// тип *URLStorage и его метод PostHandler
-func (s *URLStorage) PostHandler(w http.ResponseWriter, req *http.Request) {
-	// Автотесты не проходили на еще одном уровне switch
-	//Без этого switch проходит любой тип контента,
-	//а возвратиться может только text, получается ерунда.
-	// //Но с этим switch автотесты тоже не проходят
-	// switch req.Header.Get("Content-Type") {
-	// case "text/plain":
-	//param - тело запроса (тип []byte)
-	param, err := io.ReadAll(req.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Генерирую ответ и создаю запись в хранилище
-	response := "http://" + req.Host + generateShortURL(s, string(param))
-
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, response)
-	// default:
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	fmt.Fprint(w, "Content-Type isn`t text/plain")
-	// }
-}
-
-// !!!Попробовать пройти автотесты с этим методом!!
-// 04.05.25  прошел автотесты!
-// метод GetHandler типа *URLStorage
-// Получается, что так логичнее если нет пакета handlers !!
-// А если есть, то правильнее функция уровня пакета (?!?)
-func (s *URLStorage) GetHandler(w http.ResponseWriter, req *http.Request) {
-	//Тесты подсказали добавить проверку на метод:
-	switch req.Method {
-	case http.MethodGet:
-		// //Пока (14.04.2025) не знаю как передать PathValue при тестировании.
-		// id := req.PathValue("id")
-
-		// А вот RequestURI получается и от клиента и из теста
-		// Но получаю лишний "/"
-		id := strings.TrimPrefix(req.RequestURI, "/")
-
-		//Реализую интерфейс
-		longURL, err := GetEntry(s, id)
-
-		if err != nil {
-			//http.Error(w, "URL not found", http.StatusBadRequest)
-			w.Header().Set("Location", err.Error())
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set("Location", longURL)
-		// //И так и так работает. Оставил первоначальный вариант.
-		//http.Redirect(w, r, longURL, http.StatusTemporaryRedirect)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	default:
-		w.Header().Set("Location", "Method not allowed")
-		w.WriteHeader(http.StatusBadRequest)
-	}
 }
 
 // Реализую интерфейс Storager
