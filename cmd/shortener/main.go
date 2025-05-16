9a87c19921e7bc4de9764fc4ca59e5b7c4b39b82
Constantine <tmail99@gmail.com>
2025-05-16 15:43:20 +0300

    готовлю конфигурацию

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-
 	"github.com/dr2cc/URLsShortener.git/internal/config"
 	"github.com/dr2cc/URLsShortener.git/internal/server"
 )
 
 func main() {
+	// //из примера
+	// cfg := config.MustLoad()
+
 	// обрабатываем аргументы командной строки
 	config.ParseFlags()
-
 
 	if err := server.Run(); err != nil {
 		panic(err)
